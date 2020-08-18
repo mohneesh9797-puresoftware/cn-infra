52ede30b2d5ad48efcac531654a4cb7747d878df
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-08-18 12:00:16 +0530

    Update install-consul.sh

--- scripts/install-consul.sh
+++ scripts/install-consul.sh
@@ -2,7 +2,11 @@
 set -e
 
 CONSUL_VERSION=1.5.3
-
+if [ `uname -m` = 'aarch64' ]; then
+    download_url=https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_arm64.zip
+else
+    download_url=https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
+fi
 download_url=https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
 
 wget -nv -O /tmp/consul.zip ${download_url}
